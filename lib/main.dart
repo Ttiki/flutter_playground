ba21a3be8007f328d979b1fe7aa9d8f747716112
Ttiki <clem64.combier@gmail.com>
2022-02-25 12:42:44 +0100

    Second tutorial video. Change theme (color, font, adding style to texts, etc.)

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,28 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
         //We can't just add raw text. Flutter forces us to use a Widget Text to display text on screen.
         title: Text("Hello world!"),
+      backgroundColor: Colors.amberAccent,
       //Attribut to display our title at the center of the AppBar.
       //This attrbut forces child widgets of AppBar to be centered relative to their parent widget (here AppBar.)
       //centerTitle: true, //commented because I don't want my text to be centered
     ),
     body: Center(
-      child: Text("Hello world!"),
+      child: Text(
+          "Hello world!",
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.blueAccent,
+          fontFamily: "Din1451",
+        )
+      ),
     ),
-    floatingActionButton: Text("Click"),
+    floatingActionButton: FloatingActionButton(
+      child: Text("Click"),
+      backgroundColor: Colors.amberAccent,
+      //Actions
+      onPressed: (){},
+    ),
   ),
 ));
