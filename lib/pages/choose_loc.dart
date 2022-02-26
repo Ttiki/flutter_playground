46713a493c6fc4e11a647e439e246f4c316940a9
Ttiki <clem64.combier@gmail.com>
2022-02-26 10:59:12 +0100

    @todo test

---
+++ lib/pages/choose_loc.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ChooseLoc extends StatefulWidget {
+  const ChooseLoc({Key? key}) : super(key: key);
+
+  @override
+  _ChooseLocState createState() => _ChooseLocState();
+}
+
+class _ChooseLocState extends State<ChooseLoc> {
+
+  @override
+  void initState() {
+    //@todo: implement initState
+    super.initState();
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text("Choose a location"),
+        elevation: 0, //Make the appBar flat (no shadow at the bottom)
+      ),
+      body: Text("Hello chose location world!"),
+    );
+  }
+}
