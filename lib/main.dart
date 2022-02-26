4f96dad7112507654a552c11e1ab71eea94247c4
Clément COMBIER <clem64.combier@gmail.com>
2022-02-26 11:15:30 +0100

    Testing todo app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+//TODO test
+
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
